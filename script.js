54982cbab2a2c814952df6116618cdcafc277daf
LuzianeFreitas <freitas.lu@outlook.com>
2021-02-01 14:25:38 -0300

    add functions to data-table

--- script.js
+++ script.js
@@ -1,8 +1,78 @@
 const Modal = {
-    open(){
+    open() {
         document.querySelector('.modal-overlay').classList.add('active')
     },
-    close(){
+    close() {
         document.querySelector('.modal-overlay').classList.remove('active')
     }
 }
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '02/01/2021',
+    },
+    {
+        id: 2,
+        description: 'Criação de Website',
+        amount: 500000,
+        date: '02/01/2021',
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '02/01/2021',
+    },
+]
+
+const Transaction = {
+    incomes() {
+        // somar as entradas
+    },
+    expenses() {
+        // somar as saídas
+    },
+    total() {
+        // entradas - saídas
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+        
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclasses = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+        const html = `
+
+                <td class="description">${transaction.description}</td>
+                <td class="${CSSclasses}">${transaction.amount}</td>
+                <td class="date">${transaction.date}</td>
+                <td>
+                    <img src="./assets/minus.svg" alt="Remover transação">
+                </td>
+        `
+
+        return html
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+    }
+}
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
